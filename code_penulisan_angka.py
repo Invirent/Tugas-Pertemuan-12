1c2824a32269feba24142021903cfcbc3c8af283
Invirent <74411024+Invirent@users.noreply.github.com>
2020-11-20 20:15:11 +0700

    Add files via upload

--- code_penulisan_angka.py
+++ code_penulisan_angka.py
@@ -1,6 +1,6 @@
 import os
 kata = ['','one ','two ','three ','four ','five ','six ','seven ','eight ','nine ']
-belasan = ['','eleven','twelve','thirteen','fourteen','fifthteen','sixteen','seventeen','eighteen','nineteen']
+belasan = ['','eleven ','twelve ','thirteen ','fourteen ','fifthteen ','sixteen ','seventeen ','eighteen ','nineteen ']
 puluhan = ['','ten ','twenty ','thirty ','fourty ','fifty ','sixty ','seventy ','eighty ','ninety ']
 def perhitungan(n):
     if n <10:
@@ -23,6 +23,32 @@ def perhitungan(n):
             return belasan[n//10]
         else:
             return puluhan[n//10]+ (perhitungan(n%10) if n%10!=0 else ' ')
-nomor= int(input('Number? '))
-print  (perhitungan(nomor))
-os.system("pause")
+
+
+while True:
+    os.system("cls")
+    err=0
+    print ("Program Angka ke Huruf ")
+    print ('=======================================')
+    try:
+        nomor= int(input('Number? '))
+        print (perhitungan(nomor))
+        os.system("pause")
+        retry=input('Apakah Program ingin Diulang?(Y/N) ')
+        while True:
+            if retry.lower() in ['y','n']:
+                break
+        if retry.lower() =='n':
+            break
+    except ValueError:
+        print ('Input harus angka')
+        err+=1
+    except Exception as e:
+        print(f'Error Type {e}')
+    if err == 1:
+        retry=input('Apakah Program ingin Diulang?(Y/N) ')
+        while True:
+            if retry.lower() in ['y','n']:
+                break
+        if retry.lower() =='n':
+            break
